ab8e6d1d43966e252739db8860139e4460f1e23c
Miquel Bantulà <miki.bantula@gmail.com>
2020-10-13 17:48:18 +0200

    Missing title data in SearchMovies

--- src/components/SearchMovies.vue
+++ src/components/SearchMovies.vue
@@ -14,6 +14,12 @@
 import { mapActions } from "vuex";
 
 export default {
+  data() {
+    return {
+      title: ""
+    };
+  },
+
   methods: {
     ...mapActions(["fetchMovies"]),
 
@@ -41,7 +47,6 @@ export default {
     font-weight: 400;
     line-height: 1.5;
     color: #495057;
-    background-color: $light-gray;
     background-clip: padding-box;
     border: 1px solid #ced4da;
     border-radius: 0.25rem;
