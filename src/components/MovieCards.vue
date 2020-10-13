0a1f604aee298d83ea074c710f231e3e4b4abe1c
Miquel Bantulà <miki.bantula@gmail.com>
2020-10-13 17:47:53 +0200

    Styling movieCards component

--- src/components/MovieCards.vue
+++ src/components/MovieCards.vue
@@ -1,29 +1,41 @@
 <template>
   <div>
     <div class="movies">
-      <MovieCard v-for="(movie, idx) in getMovieResults" :key="idx" :movie="movie" />
+      <Slider>
+        <SliderItem v-for="(slide, i) in numberOfSlides" :key="i" :idx="i">
+          <MovieCard v-for="(movie, idx) in getMovieResults.slice(i, i + 4)" :key="idx" :movie="movie"></MovieCard>
+        </SliderItem>
+      </Slider>
     </div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
+
 import MovieCard from "@/components/MovieCard";
+import Slider from "@/components/slider/Slider";
+import SliderItem from "@/components/slider/SliderItem";
+
 export default {
   computed: {
-    ...mapGetters(["getMovieResults"])
+    ...mapGetters(["getMovieResults"]),
+
+    numberOfSlides() {
+        if (this.getMovieResults == null) return;
+        const numOfSlides = this.getMovieResults.length / 4;
+        if (numOfSlides >= 4) {
+            return numOfSlides;
+        }  else {
+            return 4;
+        }
+    },
   },
 
   components: {
-    MovieCard
+    MovieCard,
+    Slider,
+    SliderItem
   }
 };
 </script>
-
-<style scoped lang="scss">
-.movies {
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-  gap: 1.5rem;
-}
-</style>
